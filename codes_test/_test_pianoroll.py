fd3f3c5a6312f9992d02cec14324344d8ad8c1ee
ykykyukai <axpwyk@gmail.com>
2019-11-05 01:26:36 +0800

    2019110501
    from laptop

--- codes_test/_test_pianoroll.py
+++ codes_test/_test_pianoroll.py
@@ -3,5 +3,5 @@ from midi import *
 sheet, ticks_per_beat, _ = midi2sheet(r'../midi/test2.mid')
 pr = Pianoroll(sheet, ticks_per_beat)
 pr.set_default_ts(4, 4)
-ch = 0
-pr.draw(whdpi=(16, 6, 36), nn_interval=pr.get_nn_range(ch), tick_interval=(2*1920, pr.max_tick), splits_per_beat=4, channel=ch, is_drum=False)
+ch = 1
+pr.draw(whdpi=(24, 6, 36), nn_interval=pr.get_nn_range(ch), tick_interval=(2*1920, pr.max_tick), splits_per_beat=4, channel=ch, is_drum=False)
